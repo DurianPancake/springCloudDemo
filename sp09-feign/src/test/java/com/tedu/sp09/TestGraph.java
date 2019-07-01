ffd467ac77e7f786658c6204eb85d23790cd2ee5
LLH <562406797@qq.com>
2019-07-01 14:49:18 +0800

    向注册中心注册IP地址

--- sp09-feign/src/test/java/com/tedu/sp09/TestGraph.java
+++ sp09-feign/src/test/java/com/tedu/sp09/TestGraph.java
@@ -11,9 +11,9 @@ public class TestGraph {
 
     private static Color BACK_GROUND_COLOR = new Color(238, 238, 238);
 
-    private static int IMG_WIDTH = 240;
+    private static int IMG_WIDTH = 260;
 
-    private static int IMG_HEIGHT = 240;
+    private static int IMG_HEIGHT = 260;
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         //得到图片缓冲区
@@ -24,15 +24,12 @@ public class TestGraph {
         //得到它的绘制环境(这张图片的笔)
         Graphics2D g2 = (Graphics2D) bi.getGraphics();
 
-        g2.setBackground(BACK_GROUND_COLOR);
-        g2.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);//填充一个矩形 左上角坐标(0,0),宽70,高150;填充整张图片
         //设置颜色
-        g2.setColor(Color.WHITE);
-        g2.fillRect(0, 0, 150, 70);//填充整张图片(其实就是设置背景颜色)
-
+        g2.setBackground(BACK_GROUND_COLOR);
+        g2.setColor(Color.RED);
+        g2.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);//填充一个矩形 左上角坐标(0,0);填充整张图片
 
         g2.setColor(Color.BLACK);//设置背景颜色
-
         g2.drawString("HelloWorld", 3, 50); //向图片上写字符串
         ImageIO.write(bi,"JPG", new FileOutputStream("D://"+System.currentTimeMillis()+".jpg"));//保存图片 JPEG表示保存格式
 
